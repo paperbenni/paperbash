bc93bc9cefcb9673407a5aeae16096eca4d3ac65
paperbenni <paperbenni@gmail.com>
2018-08-15 18:58:34 +0200

    adds installation of required packages

--- install.sh
+++ install.sh
@@ -1,4 +1,42 @@
 #!/bin/bash
+
+
+	PAPERPACKAGES=$(curl https://raw.githubusercontent.com/paperbenni/paperbash/master/required.txt)
+
+if apt -v > /dev/null
+then
+
+	sudo apt update -y
+	echo "looking for required packages"
+	for PACKAGE in $PAPERPACKAGES
+	do
+		if dpkg -s "$PACKAGE"
+		then
+			echo "found $PACKAGE"
+		else
+			echo "$PACKAGE not found, installing"
+			sudo apt install -y "$PACKAGE"
+		fi
+
+	done
+fi
+
+if apk > /dev/null
+then
+	apk update
+	for PACKAGE in $PAPERPACKAGES
+	do
+		apk install "$PACKAGE"
+	done
+fi
+
+if ! (apt v- > /dev/null && apk > /dev/null)
+then
+	echo "the package manager of your os is currently not supported,
+	please install the following packages manually
+	$PAPERPACKAGES"
+fi
+
 pushd ~/
 echo "sucessfully installed paperbash"
 curl https://raw.githubusercontent.com/paperbenni/paperbash/master/setup.sh >>~/.bashrc
